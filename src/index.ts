24c80532daabd41e6b1908cadd963ca16b80ceba
Fendy Putra <fendy.putra@meeco.me>
2024-03-13 16:03:56 +1100

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export * from './issuer.js';
 export * from './types.js';
 export * from './util.js';
 export * from './verifier.js';
+
+export { SDJWTDisclosureStruct, SDJsonpath } from '@meeco/sd-jwt';
